e28314ec3406990121e92a65ce7c290e43c54f33
Felipe Navas <felipenavas.itec@gmail.com>
2024-02-11 15:35:21 -0300

    Adding implementations of repository and datasources

---
+++ src/infrastructure/datasources/auth.datasource.impl.ts
@@ -0,0 +1,25 @@
+import {
+  AuthDatasource,
+  CustomError,
+  RegisterUserDto,
+  UserEntity
+} from '../../domain';
+
+export class AuthDatasourceImpl implements AuthDatasource {
+  async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
+    const { name, email, password } = registerUserDto;
+
+    try {
+      // 1 Check if the email exist
+      // 2 Hash the password
+      // 3 Map response to the Entity
+
+      return new UserEntity('1', name, email, password, ['ADMIN_ROLE']);
+    } catch (error) {
+      if (error instanceof CustomError) {
+        throw error;
+      }
+      throw CustomError.internalServer();
+    }
+  }
+}
---
+++ src/infrastructure/index.ts
@@ -0,0 +1,2 @@
+export { AuthDatasourceImpl } from './datasources/auth.datasource.impl';
+export { AuthRepositoryImpl } from './repositories/auth.repository.impl';
