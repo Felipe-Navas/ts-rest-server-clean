6e13e310d7e69d66606efa4b19c1078cd6bc7a80
Felipe Navas <felipenavas.itec@gmail.com>
2024-02-14 16:10:19 -0300

    Add registeruser use case

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -2,4 +2,5 @@ export { AuthDatasource } from './datasources/auth.datasource';
 export { AuthRepository } from './repositories/auth.repository';
 export { CustomError } from './errors/custom.error';
 export { RegisterUserDto } from './dtos/auth/register-user.dto';
+export { RegisterUser } from './use-cases/auth/register-user.use-case';
 export { UserEntity } from './entities/user.entity';
--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express';
-import { AuthRepository, CustomError, RegisterUserDto } from '../../domain';
-import { JwtAdapter } from '../../config';
+import {
+  AuthRepository,
+  CustomError,
+  RegisterUser,
+  RegisterUserDto
+} from '../../domain';
 import { UserModel } from '../../data/mongoDB';
 
 export class AuthController {
@@ -19,16 +23,13 @@ export class AuthController {
     const [error, registerUserDto] = RegisterUserDto.create(req.body);
     if (error) return res.status(400).json({ error });
 
-    this.authRepository
-      .register(registerUserDto!)
-      .then(async (user) => {
-        res.json({
-          user,
-          token: await JwtAdapter.generateToken({ id: user.id })
-        });
+    new RegisterUser(this.authRepository)
+      .execute(registerUserDto!)
+      .then((data) => {
+        res.json(data);
       })
-      .catch((error) => {
-        this.handleError(error, res);
+      .catch((err) => {
+        this.handleError(err, res);
       });
   };
 
@@ -41,7 +42,8 @@ export class AuthController {
       .then((users) => {
         res.json({
           users,
-          user: req.body.user });
+          user: req.body.user
+        });
       })
       .catch(() => {
         res.status(500).json('Internal Server Error');
