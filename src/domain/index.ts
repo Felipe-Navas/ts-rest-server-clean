e0a96f98ac2af4eefeb708b68bfaceb8441970fb
Felipe Navas <felipenavas.itec@gmail.com>
2024-02-15 10:59:43 -0300

    Add LoginUserDto

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,6 +1,7 @@
 export { AuthDatasource } from './datasources/auth.datasource';
 export { AuthRepository } from './repositories/auth.repository';
 export { CustomError } from './errors/custom.error';
-export { RegisterUserDto } from './dtos/auth/register-user.dto';
+export { LoginUserDto } from './dtos/auth/login-user.dto';
 export { RegisterUser } from './use-cases/auth/register-user.use-case';
+export { RegisterUserDto } from './dtos/auth/register-user.dto';
 export { UserEntity } from './entities/user.entity';
